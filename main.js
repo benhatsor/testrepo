c50495bcaeb900bc3e8509460431392032e8a82a
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2021-09-30 14:01:13 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-function say(message) {
-  console.log(message);
+function typeNode(text) {
+
+    let s = window.getSelection();
+    let r0 = s.getRangeAt(0);
+
+    //let rContainer = r0.endContainer;
+    //let rOffset = r0.endOffset;
+
+    const textEl = document.createTextNode(text);
+    r0.insertNode(textEl);
+
+    s.modify('move', 'forward', 'character');
+
+    //s.setBaseAndExtent(rContainer, rOffset, rContainer, rOffset);
+
 }
 
-say('Hello world!');
-another!
