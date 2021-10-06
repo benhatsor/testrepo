8417c1b731f15bc454970a1de290fdd96084bc73
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2021-10-06 13:53:45 +0300

    Update another.js

--- another.js
+++ another.js
@@ -1,1 +1,20 @@
+hljs.debugMode = function() { SAFE_MODE = false; };
+  hljs.safeMode = function() { SAFE_MODE = true; };
+  hljs.versionString = packageJSON.version;
 
+  for (const key in MODES) {
+    // @ts-ignore
+    if (typeof MODES[key] === "object") {
+      // @ts-ignore
+      deepFreeze(MODES[key]);
+    }
+  }
+
+  // merge all the modes/regexes into our main object
+  Object.assign(hljs, MODES);
+
+  return hljs;
+};
+
+// export an "instance" of the highlighter
+export default HLJS({});
