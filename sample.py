96266272b0fb712b47485be71643e90ca608399d
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-11-10 15:02:37 +0200

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,4 +1,20 @@
-def sayhello():
-  print('hello world!')
-  
-sayhello()
+import turtle
+
+s = turtle.getscreen()
+t = turtle.Turtle()
+
+t.pendown()
+t.right(90)
+t.pensize(5)
+t.forward(100)
+t.pencolor("green")
+
+t.penup()
+t.right(90)
+t.forward(200)
+t.pencolor("blue")
+t.pendown()
+t.forward(100)
+t.penup()
+
+
