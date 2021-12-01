3f312a5db5812507ab5b01d463da28da758ad24a
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2021-12-01 13:42:30 +0200

    Update axios.js

--- axios.js
+++ axios.js
@@ -1,2 +1,23 @@
+//Http request function:
+var axios = {
+  
+  'post': (url, data, options) => {
+    return new Promise((resolve, reject) => {
+      try {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function () {
+          if (this.readyState == 4 && (this.status == 201 || this.status == 200)) {
+            resolve(JSON.parse(this.responseText));
+          }
+        };
 
-
+        cors = '';
+
+        xmlhttp.open('POST', (cors + url), true);
+
+        xmlhttp.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+        xmlhttp.send(JSON.stringify(data));
+      } catch(e) { reject(e) }
+    });
+  }
+};
