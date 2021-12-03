ffdf7787540c076af15929384bdc78ec0235d53e
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2021-12-03 16:30:50 +0200

    Update module-support.js

--- module-support.js
+++ module-support.js
@@ -1,3 +1,4 @@
+//Special api for loading big code files from git (bigger than 1MB)
 const resp = await axios.get('https://api.github.com/repos/KostaMalsev/three.js/git/blobs/ae2e54134ca8c69c812aa41252fd9bf84e82400b');
 
 //Replace the relative path with base64 encoded module code.
