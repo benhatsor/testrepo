ee43b002bbaf1b2cf0f1727e889e2ac4272020df
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2021-12-03 16:30:04 +0200

    Update module-support.js

--- module-support.js
+++ module-support.js
@@ -1,8 +1,9 @@
 const resp = await axios.get('https://api.github.com/repos/KostaMalsev/three.js/git/blobs/ae2e54134ca8c69c812aa41252fd9bf84e82400b');
 
+//Replace the relative path with base64 encoded module code.
 addScript(
-document.querySelector('.live-frame').contentDocument, 
-$0.innerHTML.replace('../build/three.module.js', 
-'data:text/javascript;base64,' + 
-encodeUnicode(encodeURIComponent(decodeUnicode(resp.content)))),
-'', 'module')
+  document.querySelector('.live-frame').contentDocument, 
+  $0.innerHTML.replace('../build/three.module.js', 
+  'data:text/javascript;base64,' + 
+  encodeUnicode(encodeURIComponent(decodeUnicode(resp.content)))),
+  '', 'module')
