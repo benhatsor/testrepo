6837ce5b6e29bc532b4fa254a5963cff9a1e5b86
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2021-12-03 16:27:10 +0200

    Update module-support.js

--- module-support.js
+++ module-support.js
@@ -1,3 +1,5 @@
+const resp = await axios.get('https://api.github.com/repos/KostaMalsev/three.js/git/blobs/ae2e54134ca8c69c812aa41252fd9bf84e82400b');
+
 addScript(
 document.querySelector('.live-frame').contentDocument, 
 $0.innerHTML.replace('../build/three.module.js', 
