522e6208936440244946fa6c2cef45354f4c1ae8
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2021-12-03 16:23:28 +0200

    Update module-support.js

--- module-support.js
+++ module-support.js
@@ -1,2 +1,6 @@
-
-
+addScript(
+document.querySelector('.live-frame').contentDocument, 
+$0.innerHTML.replace('../build/three.module.js', 
+'data:text/javascript;base64,' + 
+encodeUnicode(encodeURIComponent(decodeUnicode(resp.content)))),
+'', 'module')
