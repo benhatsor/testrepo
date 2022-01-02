0497964063de6deccfb986a461b0e13c2aae6c78
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-01-02 20:18:27 +0200

    Update callbackasync.js

--- callbackasync.js
+++ callbackasync.js
@@ -1,2 +1,28 @@
 
-
+
+async function pushAllChunksToAWS(chunks) {
+  
+  asyncForEach(chunks, (newChunk) => {
+    
+    await pushChunkToAWS(newChunk);
+    
+  });
+  
+}
+
+function pushChunkToAWS(chunk) {
+  
+  return new Promise(resolve => {
+  
+    pushToAWS(chunk, resolve);
+    
+  });
+  
+}
+
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+}
+
