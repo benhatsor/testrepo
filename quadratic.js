3b3ca1241b19ff173daf5e55017d1f9aa39bc941
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-01-14 11:09:35 +0200

    Update quadratic.js

--- quadratic.js
+++ quadratic.js
@@ -1,2 +1,15 @@
 
-
+function calculateQuad(a, b, c) {
+  
+  // discriminant
+  const disc = Math.pow(b, 2) - (4 * a * c);
+  
+  // quadratic formula
+  // 2 responses
+  const res1 = ((b * -1) + Math.sqrt(disc)) / (2 * a);
+  const res2 = ((b * -1) - Math.sqrt(disc)) / (2 * a);
+  
+  return [res1, res2];
+  
+}
+
