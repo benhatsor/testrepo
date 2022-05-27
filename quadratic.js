d0bf917e9544a62f6a57942207636d14ef65cebc
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-05-27 09:29:46 +0300

    Update quadratic.js

--- quadratic.js
+++ quadratic.js
@@ -4,8 +4,6 @@ function calculateQuad(a, b, c) {
   // discriminant
   const disc = Math.pow(b, 2) - (4 * a * c)
   
-  if (disc < 0) return ['disc is negative'];
-  
   // quadratic formula
   // 2 responses
   const res1 = ((b * -1) + Math.sqrt(disc)) / (2 * a);
