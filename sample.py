f1f65fb3109e4752cb31c1325d7a3f07099442a7
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2023-01-03 14:41:44 +0200

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,19 +1,16 @@
 
-maxNum = int(input("Max num: "));
+maxNum = int(input("Max num: "))
 
-fizzNum = int(input("Fizz num: "));
-buzzNum = int(input("Buzz num: "));
+fizzNum = int(input("Fizz num: "))
+buzzNum = int(input("Buzz num: "))
 
-for (i = 0; i < maxNum; i++) {
+for i in range(maxNum):
   
-  if (i % fizzNum == 0 && i % buzzNum == 0) {
-    print("Fizzbuzz");
-  } else if (i % fizzNum == 0) {
-    print("Fizz");
-  } else if (i % buzzNum == 0) {
-    print("Buzz");
-  } else {
-    print(i);
-  }
-  
-}
+  if (i % fizzNum == 0 && i % buzzNum == 0):
+    print("Fizzbuzz")
+  else if (i % fizzNum == 0):
+    print("Fizz")
+  else if (i % buzzNum == 0):
+    print("Buzz")
+  else:
+    print(i)
