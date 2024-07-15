f4a5b8f3e7b903814d2264e376b7a8dfe4c196ce
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2024-07-15 15:51:06 +0300

    Update parsingCSV.js

--- parsingCSV.js
+++ parsingCSV.js
@@ -1,9 +1,71 @@
 
 // data = CSV file contents (text)
+// data prep: in Excel, remove all thousands seperators from numbers.
 
 data = data.split('\n');
 
 data.forEach((item, index) => {
   data[index] = item.trim().replaceAll(' ', '').replaceAll('\t', '').replaceAll('"', '').split(',');
+});
+
+
+
+let out = {};
+
+let currGroup;
+
+data.forEach((row, rowIndex) => {
+
+  if (rowIndex === 0) return;
+  
+  row.forEach((column, colIndex) => {
+
+    if (colIndex === (row.length - 1)) return;
+
+    // age group
+    if (colIndex === 0 && column !== '') {
+
+      currGroup = column;
+      
+    }
+
+    if (colIndex === 1 && column === '') {
+
+      return;
+
+    }
+
+    // years
+    if (colIndex === 1) {
+
+      column = column.replaceAll('+', '');
+      
+      const [min, max] = column.split('-');
+
+      out[currGroup] = {
+        age: {
+          min: min,
+          max: max
+        }
+      };
+
+    }
+
+    // sed
+    if (colIndex === 2) {
+
+      out[currGroup].sed = column;
+
+    }
+
+    // active
+    if (colIndex === 3) {
+
+      out[currGroup].active = column;
+
+    }
+    
+  });
+  
 });
 
