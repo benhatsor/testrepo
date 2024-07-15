912302f65239061ec3989c1422a5c0ad5ebe76bf
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2024-07-15 15:32:32 +0300

    Update parsingCSV.js

--- parsingCSV.js
+++ parsingCSV.js
@@ -1,2 +1,9 @@
 
-
+// data = CSV file contents (text)
+
+data = data.split('\n');
+
+data.forEach((item, index) => {
+  data[index] = item.trim().replaceAll(' ', '').replaceAll('\t', '').replaceAll('"', '');
+});
+
